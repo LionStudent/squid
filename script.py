6cffe6bd82ef1c07dbbb11a40206846255d94710
squidstudent <squid.student@protonmail.com>
2020-05-12 22:06:24 +0000

    graphed the sine function

--- script.py
+++ script.py
@@ -1,11 +1,17 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import math
 
 # Cofigure matplotlib to write PNG files
 matplotlib.use('Agg')
 
 # Get some x-values
-xAxis = list(range(-20,20))
+xAxis = list(range(0,64))
+
+xTenth = []
+for x in xAxis:
+  xTenth.append(x*0.1)
+xAxis = xTenth
 
 # Write a function to build our y-values
 def parabola(xValues):
@@ -14,7 +20,8 @@ def parabola(xValues):
   # get each number in the given list of x-values
   for x in xValues:
     # compute the square of each value
-    y = x*x
+    #y = x*x
+    y = math.sin(x)
     # add the squared value to the list of results
     yValues.append(y)
   # give the resultant list of squared valuse to the caller of this function 
@@ -25,17 +32,17 @@ yAxis = parabola(xAxis)
 
 # We don't have to use all the data we create. We 
 # can "slice" out protions that we want.
-sliceXAxis  = xAxis[5:35]
-sliceYAxis  = yAxis[5:35]
+sliceXAxis  = xAxis[:]
+sliceYAxis  = yAxis[:]
 
 # Plot data points as a red circle.
-style = 'ro'
+style = 'b-'
 
 # Give matplotlib our data
 plt.plot(sliceXAxis , sliceYAxis , style)
 
 # Specify the region of the plot we wish to display
-plt.axis([-10, 10, 0, 100])
+plt.axis([0, 6.4, -1, 1])
 
 # Save our plot to the local disk drive
 filename = 'graph.png'
